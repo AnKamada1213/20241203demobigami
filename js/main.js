7be0196e72abf95f8aaee26fb3eec1f868bac615
AnKamada1213 <139840601+AnKamada1213@users.noreply.github.com>
2024-12-03 15:26:42 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,61 +24,54 @@ $(function () {
     $('#header').removeClass('open');
   });
 
-  // PageTop と Line ボタン
-  function PageTopAnime() {
-    var scroll = $(window).scrollTop();
-    var documentHeight = $(document).height();
-    var windowHeight = $(window).height();
-    var offset = 200;
+  
+// PageTop と Line ボタンの表示/非表示制御
+function PageTopAnime() {
+  const scroll = $(window).scrollTop();
+  const documentHeight = $(document).height();
+  const windowHeight = $(window).height();
+  const offset = 200;
 
-    if (scroll >= window.innerHeight && scroll <= documentHeight - windowHeight - offset) {
-      $('#page-top').removeClass('DownMove FadeOut').addClass('UpMove');
-      $('#line').removeClass('DownMove FadeOut').addClass('UpMove');
-      $('#instagram').removeClass('DownMove FadeOut').addClass('UpMove');
-
-    } else if (scroll > documentHeight - windowHeight - offset) {
-      $('#page-top').removeClass('UpMove').addClass('FadeOut');
-      $('#line').removeClass('UpMove').addClass('FadeOut');
-      $('#instagram').removeClass('UpMove').addClass('FadeOut');
-    } else {
-      $('#page-top').removeClass('UpMove FadeOut').addClass('DownMove');
-      $('#line').removeClass('UpMove FadeOut').addClass('DownMove');
-      $('#instagram').removeClass('UpMove FadeOut').addClass('DownMove');
-    }
+  if (scroll >= window.innerHeight) {
+    // 最初のスクロール以降に表示
+    $('#page-top').removeClass('Hidden DownMove FadeOut').addClass('UpMove');
+    $('#line').removeClass('Hidden DownMove FadeOut').addClass('UpMove');
+    $('#instagram').removeClass('Hidden DownMove FadeOut').addClass('UpMove');
+  } else {
+    // 初期スクロールが行われるまで非表示
+    $('#page-top').addClass('Hidden');
+    $('#line').addClass('Hidden');
+    $('#instagram').addClass('Hidden');
   }
 
-  // 初期表示時に PageTopAnime 関数を実行
+  if (scroll > documentHeight - windowHeight - offset) {
+    // ページの一番下付近でフェードアウト
+    $('#page-top').removeClass('UpMove').addClass('FadeOut');
+    $('#line').removeClass('UpMove').addClass('FadeOut');
+    $('#instagram').removeClass('UpMove').addClass('FadeOut');
+  } else {
+    // フェードアウト解除
+    $('#page-top').removeClass('FadeOut');
+    $('#line').removeClass('FadeOut');
+    $('#instagram').removeClass('FadeOut');
+  }
+}
+
+// 初期状態で非表示設定
+$(document).ready(function () {
   PageTopAnime();
+});
 
-  $(window).scroll(PageTopAnime);
+// スクロールイベント
+$(window).scroll(PageTopAnime);
 
-  $('#page-top a').click(function () {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 600);
-    return false;
-  });
-
-  // フェードインエフェクト
-  function fadeInEffect(selector, offset) {
-    $(selector).each(function () {
-      let scroll = $(window).scrollTop();
-      let target = $(this).offset().top;
-      let windowHeight = $(window).height();
-      if (scroll > target - windowHeight + offset) {
-        $(this).css({
-          'opacity': '1',
-          'transform': 'translateY(0)'
-        });
-      }
-    });
-  }
-
-  // フェードイン効果を適用
-  $(window).scroll(function () {
-    fadeInEffect('.attempt_box', 150);
-    fadeInEffect('.state_list_box', 300);
-  });
+// ページトップボタンのクリックでスムーズスクロール
+$('#page-top a').click(function () {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 600);
+  return false;
+});
 
   // アコーディオンエリア
   $('.qa_title').on('click', function () {
